52c4f146b2d00eb7f4f2685d4b24a91e1ad45ff1
Mark Ransom <megamark16@gmail.com>
2011-03-02 15:07:26 -0600

    Added a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name = "cuisine",
+	version = "0.0.1",
+	description = "Chef-like functionality for Fabric",
+	author = "Sebastien Pierre",
+	author_email = "sebastien.pierre@gmail.com",
+	url = "https://github.com/sebastien/cuisine",
+	#packages=["cuisine",],
+	install_requires=['fabric',],
+	py_modules = ['cuisine',],
+	)
