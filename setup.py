a78c8c246890e5f234a9cc063704c3d28ede071f
cgilmer <cgilmer@beta.(none)>
2011-03-04 10:13:32 -0800

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name = "cuisine",
+	packages = ["cuisine"],
+	version = "0.0.1",
+	description = "Chef-like functionality for Fabric",
+	author = "Sebastien Pierre",
+	author_email = "sebastien.pierre@gmail.com",
+	url = "http://type-z.org/sebastien",
+	download_url = "https://github.com/sebastien/cuisine",
+	keywords = ["fabric", "chef", "ssh",],
+	classifiers = [
+		"Programming Language :: Python",
+		"Development Status :: 3 - Alpha",
+		"Natural Language :: English",
+		"Environment :: Web Environment",
+		"Intended Audience :: Developers",
+		"Operating System :: OS Independent",
+		"Topic :: Utilities"
+		],
+)
+
