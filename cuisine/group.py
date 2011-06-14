8d7a4ed6eb1e6244838c68df33c283e10119f083
Sergei Lebedev <superbobry@gmail.com>
2011-06-14 19:48:53 +0400

    Cleaned up `cuisine.group`

--- cuisine/group.py
+++ cuisine/group.py
@@ -9,71 +9,99 @@
     :license: BSD, see LICENSE for more details.
 """
 
-__all__ = ["create", "check", "ensure", "member", "add_user", "ensure_user"]
+__all__ = ["create", "exists", "get", "ensure", "has", "add_user", "ensure_user"]
+
+import os
 
 from fabric.api import sudo
 
-from . import multiargs, run
+from . import multiargs, run, file
 
 
 def create(name, gid=None):
-	"""Creates a group with the given name, and optionally given gid."""
-	options = []
-	if gid:  options.append("-g '%s'" % (gid))
-	sudo("groupadd %s '%s'" % (" ".join(options), name))
+    """Creates a new remote group.
+
+    :param unicode name: name for the group being created.
+    :param int gid: numerical value of the group's ID.
+    :raises ValueError: if a group with a given name already exists.
+    """
+    if exists(name):
+        raise ValueError("Group %r already exists.")
+
+    if gid:
+        sudo("groupadd -g %r %s" % (gid, name))
+    else:
+        sudo("group add %s" % name)
 
 
-def check(name):
-	"""Checks if there is a group defined with the given name, returning its information
-	as a '{"name":<str>,"gid":<str>,"members":<list[str]>}' or 'None' if the group
-	does not exists."""
-	group_data = run("cat /etc/group | egrep '^%s:' ; true" % (name))
-	if group_data:
-		name,_,gid,members = group_data.split(":",4)
-		return dict(name=name,gid=gid,members=tuple(m.strip() for m in members.split(",")))
-	else:
-		return None
+def exists(name):
+    """Returns ``True`` if there is a remote group with a given name
+    and ``False`` otherwise.
+    """
+    return bool(run("cat /etc/group | egrep '^%s:' ; true" % name))
+
+
+def get(name, silent=True):
+    """Returns group data from ``/etc/group`` on the remote machine.
+    or ``None`` if a given group doesn't exist.
+
+    :param unicode name: name of the group to fetch data for.
+    :param bool silent: if ``False`` :exc:`ValueError` is raised when
+                        no group is found for a given name otherwise
+                        ``None`` is returned.
+    """
+    data = run("cat /etc/group | egrep '^%s:' ; true" % name)
+    if data:
+        data = dict(zip(["name", "password", "gid", "members"], data))
+        data["members"] = [m.strip() for m in data["members"]]
+        return data
+    elif not silent:
+        raise ValueError("Group %r doesn't exist.")
 
 
 def ensure(name, gid=None):
-	"""Ensures that the group with the given name (and optional gid) exists."""
-	d = check(name)
-	if not d:
-		create(name, gid)
-	else:
-		if gid != None and d.get("gid") != gid:
-			sudo("groupmod -g %s '%s'" % (gid, name))
+    """Ensures that the group with the given name (and optional gid)
+    exists.
+    """
+    data = get(name)
+    if not data:
+        create(name, gid)
+    elif gid is not None and data.get("gid") is not gid:
+        sudo("groupmod -g %s %r" % (gid, name))
 
-def member(group, user):
-	"""Checks if the given user is a member of the given group. It will return 'False'
-	if the group does not exist."""
-	d = check(group)
-	if d is None:
-		return False
-	else:
-		return user in d["members"]
+
+def has(group, user):
+    """Return ``True`` if a given user is a member of the given group
+    and ``False`` otherwise.
+
+    :raises ValueError: if a group with a given name doesn't exist.
+    """
+    return user in get(group, silent=False)["members"]
+
 
 @multiargs
 def add_user(group, user):
-	"""Adds the given user/list of users to the given group/groups."""
-	from . import file
+    """Adds the given user to a given group.
 
-	assert check(group), "Group does not exist: %s" % (group)
-	if not member(group, user):
-		lines = []
-		for line in file.read("/etc/group").split("\n"):
-			if line.startswith(group + ":"):
-				if line.strip().endswith(":"):
-					line = line + user
-				else:
-					line = line + "," + user
-			lines.append(line)
-		text = "\n".join(lines)
-		file.write("/etc/group", text)
+    :raises ValueError: if a group with a given name doesn't exist.
+    """
+    if not has(group, user):
+        lines = []
+        for line in file.read("/etc/group").splitlines():
+            if line.startswith(group + ":"):
+                if line.strip().endswith(":"):
+                    line = line + user
+                else:
+                    line = line + "," + user
+            lines.append(line)
+
+        file.write("/etc/group", os.sep.join(lines))
 
 
 def ensure_user(group, user):
-	"""Ensure that a given user is a member of a given group."""
-	d = check(group)
-	if user not in d["members"]:
-		add_user(group, user)
+    """Ensures that a given user is a member of a given group.
+
+    :raises ValueError: if a group with a given name doesn't exist.
+    """
+    if user not in get(group)["members"]:
+        add_user(group, user)
