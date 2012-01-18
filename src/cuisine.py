f25cc50b3dbe471b6cb28ef77fcfbd5c9cd87fdd
Florian Hines <florian.hines@gmail.com>
2012-01-18 23:40:33 +0000

    re-implement system_uuid_alias_add syntax fix for python2.6
    fixes #35

--- src/cuisine.py
+++ src/cuisine.py
@@ -593,14 +593,15 @@ def upstart_ensure(name):
 		sudo("service %s start" % name)
 
 def system_uuid_alias_add():
-	"""Adds system UUID alias to /etc/hosts.
-	Some tools/processes rely/want the hostname as an alias in
-	/etc/hosts e.g. `127.0.0.1 localhost <hostname>`.
-	"""
-	with mode_sudo(), cd('/etc'):
-			old = "127.0.0.1 localhost"
-			new = old + " " + system_uuid()
-			file_update('hosts', lambda x: text_replace_line(x, old, new)[0])
+    """Adds system UUID alias to /etc/hosts.
+    Some tools/processes rely/want the hostname as an alias in
+    /etc/hosts e.g. `127.0.0.1 localhost <hostname>`.
+    """
+    with mode_sudo(): 
+        with cd('/etc'):
+            old = "127.0.0.1 localhost"
+            new = old + " " + system_uuid()
+            file_update('hosts', lambda x: text_replace_line(x, old, new)[0])
 
 def system_uuid():
 	"""Gets a machines UUID (Universally Unique Identifier)."""
