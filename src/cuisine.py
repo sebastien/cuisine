2b947b18a90e2f35d228516fd354101b3f79911d
Sebastien Pierre <sebastien.pierre@gmail.com>
2013-01-21 12:20:49 +1300

    Setting proper revision number

--- src/cuisine.py
+++ src/cuisine.py
@@ -43,7 +43,7 @@ from __future__ import with_statement
 import base64, zlib, hashlib, os, re, string, tempfile, subprocess, types, functools, StringIO
 import fabric, fabric.api, fabric.operations, fabric.context_managers
 
-VERSION         = "0.5.3"
+VERSION         = "0.5.0"
 RE_SPACES       = re.compile("[\s\t]+")
 MAC_EOL         = "\n"
 UNIX_EOL        = "\n"
