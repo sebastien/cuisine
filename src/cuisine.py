409e17db34466dd2abdfd7c2c03cb4d32ceab0dc
Hajimu UMEMOTO <ume@mahoroba.org>
2016-06-14 16:24:33 +0900

    make package_ensure_pkgng() actually detects new install

--- src/cuisine.py
+++ src/cuisine.py
@@ -1315,7 +1315,7 @@ def package_install_pkgng(package, update=False):
 def package_ensure_pkgng(package, update=False):
 	# I am gonna have to do something different here
 	status = run("pkg info %s ; true" % package)
-	if status.stderr.find("No package(s) matching") != -1 or status.find(package) == -1:
+	if status.find("No package(s) matching") != -1 or status.find(package) == -1:
 		package_install_pkgng(package, update)
 		return False
 	else:
