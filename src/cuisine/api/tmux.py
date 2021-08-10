665ab694a1c25b97575874ac9169524848fa9541
Sébastien Pierre <sebastien.pierre@nzx.com>
2021-08-10 13:03:07 +1200

    [Fix] tmux: integers intead of strings for windows

--- src/cuisine/api/tmux.py
+++ src/cuisine/api/tmux.py
@@ -1,7 +1,7 @@
 from ..api import APIModule
 from ..decorators import logged, dispatch, variant, expose
 from ..connection.tmux import Tmux, TmuxConnection
-from typing import List
+from typing import List, Optional
 
 
 class TmuxAPI(APIModule):
@@ -18,11 +18,18 @@ class TmuxAPI(APIModule):
         return self._tmux.session_list()
 
     @expose
-    def tmux_window_list(self, session: str) -> List[str]:
+    def tmux_window_list(self, session: str) -> List[int]:
         return self._tmux.window_list(session)
 
     @expose
-    def tmux_is_responsive(self, session: str, window: str) -> bool:
+    def tmux_has(self, session: str, window: Optional[int]) -> bool:
+        if window is None:
+            return self._tmux.session_has(session)
+        else:
+            return self._tmux.window_has(session, window)
+
+    @expose
+    def tmux_is_responsive(self, session: str, window: int) -> Optional[bool]:
         return self._tmux.is_responsive(session, window)
 
 
